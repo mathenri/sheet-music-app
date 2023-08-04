19673160aa123ad57130d3f7ebe4f9e8be855256
Mattias Henriksson <henriksson.mj@gmail.com>
2023-08-04 08:12:16 +0200

    Add pokemon theme song

--- songs.js
+++ songs.js
@@ -2,7 +2,7 @@ const spanienSong = `
     X:1
     L:1/4
     K:C
-    | C D E2 | C D E2 | D D D D | C2 C2
+    | C D E2 | C D E2 | D D D D | C2 C2 |
 `
 
 const blinkaLillaStjarnaSong = `
@@ -11,13 +11,18 @@ const blinkaLillaStjarnaSong = `
     K:C
     | C C G G | A A G2 | F F E E | D D C2
     | G G F F | E E D2 | G G F F | E E D2
-    | C C G G | A A G2 | F F E E | D D C2
+    | C C G G | A A G2 | F F E E | D D C2 |
 `
 
 const pokemonSong = `
     X:1
-    L: 1/4
-    K: C
-    L: 1/8
-    | z2 E E E2 E2 | 
+    L:1/4
+    M:4/4
+    K:C
+      | z E E E | D D D2 | E E D D | D4 |
+      z F F F | E D C2 | E E D D | E4 :|
+      z2 E G | A E G A | G4 | z A A c | B A G2 |
+      A E G A | G2 C D | E E D D | E2 E G |
+      A E G A | G4 | z A A c | B A G2 |
+      E c c c | E c c2 | c2 c2 | B4 | E G A2 |
 `
