701355c6a1409c0c6b7c31efa53b424e7b793989
Mattias Henriksson <mattias.henriksson@sodra.com>
2023-08-04 07:58:03 +0200

    move song notation to separate file

---
+++ songs.js
@@ -0,0 +1,23 @@
+const spanienSong = `
+    X:1
+    L:1/4
+    K:C
+    | C D E2 | C D E2 | D D D D | C2 C2
+`
+
+const blinkaLillaStjarnaSong = `
+    X:1
+    L:1/4
+    K:C
+    | C C G G | A A G2 | F F E E | D D C2
+    | G G F F | E E D2 | G G F F | E E D2
+    | C C G G | A A G2 | F F E E | D D C2
+`
+
+const pokemonSong = `
+    X:1
+    L: 1/4
+    K: C
+    L: 1/8
+    | z2 E E E2 E2 | 
+`
